1d1fa1e1e5bc6367fa5994befdff64d5c1e72d9b
MehulCUB <mehulkumar.patel@colorado.edu>
2021-09-12 22:16:43 -0600

    updating

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,99 +1,214 @@
 #include "systemcalls.h"
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <errno.h>
+#include <fcntl.h>
+/** 
+	Author : Mehul
+	completed - modified given assignment code 
+
+	do_system - execute with system()
+	do_exec - create child process usign fork, execv and exectue command in child - wait for child to complete in parent -  get exit status 
+	do_exec_redirect -same like do_exec, redirect standard out to a file specified by outputfile, The rest of the behaviour is same as do_exec()
+
+	refence source code :The Linux Programming Interface
+	
+	Chapter 24: Process Creation			 https://man7.org/tlpi/code/online/all_files_by_chapter.html#ch24
+	Chapter 26: Monitoring Child Processes   https://man7.org/tlpi/code/online/all_files_by_chapter.html#ch26
+*/ 
+
 /**
  * @param cmd the command to execute with system()
  * @return true if the commands in ... with arguments @param arguments were executed 
  *   successfully using the system() call, false if an error occurred, 
  *   either in invocation of the system() command, or if a non-zero return 
  *   value was returned by the command issued in @param.
-*/
+ */
 bool do_system(const char *cmd)
 {
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success 
- *   or false() if it returned a failure
-*/
-
-    return true;
+	//Call the system() function with the command set in the cmd
+	//return a boolean true if the system() call completed with success false if it returned a failure
+	 
+	int ret = system(cmd);
+	if(ret == 0 && cmd==NULL)
+		return false;
+	else if (ret == -1 || ret ==127)
+		return false;
+	else		
+		return true;
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the 
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables are command to execute.
+ *   followed by arguments to pass to the command
+ *   Since exec() does not perform path expansion, the command to execute needs
+ *   to be an absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in execv()
+ * @return true if the command @param ... with arguments @param arguments were executed successfully
+ *   using the execv() call, false if an error occurred, either in invocation of the 
+ *   fork, waitpid, or execv() command, or if a non-zero return value was returned
+ *   by the command issued in @param arguments with the specified arguments.
+ */
 
 bool do_exec(int count, ...)
 {
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
-    }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+	va_list args;
+	va_start(args, count);
+	char * command[count+1];
+	int i;
+	for(i=0; i<count; i++)
+	{
+		command[i] = va_arg(args, char *);
+	}
+	command[count] = NULL;
+	// this line is to avoid a compile warning before your implementation is complete
+	// and may be removed
+	command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *   
-*/
 
-    va_end(args);
+	int return_Val;
+	int child_status;
 
-    return true;
+	pid_t pid;
+	pid = fork();  
+	//fork will create child duplicate like parent process  -it will exectue code below fork 
+	//parent gets child's pid>0
+	//and in child process pid = 0 
+
+	//-ve value  : child could not be created 
+	if (pid == -1)
+	{
+		va_end(args);
+		return false;
+	}
+	//child created and exectue command usign execv() in child process
+	else if(pid == 0)
+	{	
+		//if current process child is exectue 
+		int ret = execv(command[0], command);
+
+		//command[0] as the full path to the command to execute
+		//using the remaining arguments as second argument to the execv() command
+		if(ret == -1)
+			exit(-1);
+	}
+
+	//parent wait for child 
+	if(pid > 0)
+	{		
+		if (waitpid (pid, &child_status, 0) == -1) //wait for child 
+		{
+			return_Val = false; 
+		}
+		else
+		{
+			if (WIFEXITED(child_status))
+			{
+				if (WEXITSTATUS (child_status) == 0)
+				{
+					return_Val = true; 
+				}
+				else 
+				{
+					return_Val = false; 	
+				}
+			}
+			else 
+			{
+				return_Val = false;
+			}
+		}
+	}		
+
+	va_end(args);
+
+	return return_Val;
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.  
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
+ * @param outputfile - The full path to the file to write with command output.  
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
-    }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+	int return_Val;
+	int child_status;
+
+	va_list args;
+	va_start(args, count);
+	char * command[count+1];
+	int i;
+	for(i=0; i<count; i++)
+	{
+		command[i] = va_arg(args, char *);
+	}
+	command[count] = NULL;
+	// this line is to avoid a compile warning before your implementation is complete
+	// and may be removed
+	command[count] = command[count];
 
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *   
-*/
+	//o/p redirection refernce : https://stackoverflow.com/a/13784315/1446624
+	pid_t pid;
 
-    va_end(args);
-    
-    return true;
+	//redirecting standard out to a file specified by outputfile
+	int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+	dup2(fd, 1);  //redirect stdout=> fd - file 
+
+
+	pid = fork(); //fork will create child duplicate like parent process
+	if(pid == -1) //child could not be created 
+	{
+		return_Val = 0;
+	}
+	else if(pid ==0) //if current process is child pid = 0 
+	{
+		//exectue command in child 
+		int ret = execv(command[0],command);
+		if(ret ==-1)
+			exit(-1);
+	}
+
+	//execution in parent (parent pid>0) 
+	// parent wait to get child status  
+	if(pid > 0)
+	{		
+		if (waitpid (pid, &child_status, 0) == -1) //wait for child 
+		{
+			return_Val = false; 
+		}
+		else
+		{
+			if (WIFEXITED(child_status))
+			{
+				if (WEXITSTATUS (child_status) == 0) //returns true if the child terminated normally,
+				{
+					return_Val = true; 	
+				}
+				else 
+				{
+					return_Val = false; 	
+				}
+			}
+			else 
+			{
+				return_Val = false;
+			}
+		}
+	}		
+
+	va_end(args);
+	close(fd); //close file safely 
+	return return_Val;
 }
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,8 +11,12 @@ KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE=aarch64-none-linux-gnu- 
+#writer, finder-test utlities directory path
+UTILITY_DIR=$(pwd)
+#UTILITY_DIR=/home/mehul/Desktop/f21_aesd/assignment-1-MehulCUB/finder-app 
 
+ 
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -22,6 +26,15 @@ else
 fi
 
 mkdir -p ${OUTDIR}
+
+#fail and exit if directoy not present and then can not be created 
+if [ $? -eq 0 ]; then
+	echo "directory create : successful"	 
+else
+	echo error
+	echo "directory create : failed"	 
+	exit 1
+fi 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -35,9 +48,21 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	# building cross compiled kernel image, module and database using aarch64-none-linux-gnu-gcc compiler 
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+	
+	make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all	
+	
+	make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi 
 
+#TODO: Add Image to outdir
 echo "Adding the Image in outdir"
+
+cp "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" "${OUTDIR}"
+
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +72,17 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
+cd "$OUTDIR"
+
 # TODO: Create necessary base directories
+echo "Creating base directories"
+
+mkdir rootfs
+cd rootfs
+mkdir bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir usr/bin usr/lib usr/sbin
+mkdir -p var/log
+tree -d #print file system tree 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +91,73 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+	echo "Configuring busybox"
+	make distclean
+	make defconfig
 else
-    cd busybox
+    cd busybox  #if already present 
 fi
 
 # TODO: Make and insatll busybox
+echo "insatlling busybox"
+
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
+
+echo "busybox installation complete"
+
 # TODO: Add library dependencies to rootfs
+echo "Adding Library dependencies to rootfs/lib"
+
+cd ${OUTDIR}/rootfs
+
+export SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+echo $SYSROOT
+
+sudo cp -L $SYSROOT/lib/ld-linux-aarch64.* ${OUTDIR}/rootfs/lib/
+sudo cp -L $SYSROOT/lib64/libm.so.* ${OUTDIR}/rootfs/lib64/
+sudo cp -L $SYSROOT/lib64/libresolv.so.* ${OUTDIR}/rootfs/lib64/
+sudo cp -L $SYSROOT/lib64/libc.so.* ${OUTDIR}/rootfs/lib64/
+
+echo " Library dependencies added rootfs successful"
 
 # TODO: Make device nodes
+echo "Make device nodes"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
+#list created device node 
+ls -l dev 
+
+cd ${UTILITY_DIR}  ##move to utility directory
 # TODO: Clean and build the writer utility
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
+#copy utlitiles from finder-app directories to rootfs/home 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer ${OUTDIR}/rootfs/home/
+cp finder.sh ${OUTDIR}/rootfs/home/
+cp finder-test.sh ${OUTDIR}/rootfs/home/
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp conf/ -r ${OUTDIR}/rootfs/home/
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
+
 
 # TODO: Create initramfs.cpio.gz
+echo "Creating initramfs.cpio.gz"
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ..
+gzip -f initramfs.cpio
+
